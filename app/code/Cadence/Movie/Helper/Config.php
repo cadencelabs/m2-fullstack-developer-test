0d708332b4164d68d7860bc7e075d11fff97528c
Javier Vasquez <jvasquez@cadence-labs.com>
2022-03-24 10:14:35 -0700

    Initial commit of code.

---
+++ app/code/Cadence/Movie/Helper/Config.php
@@ -0,0 +1,8 @@
+<?php
+namespace Cadence\Movie\Helper;
+
+class Config
+{
+    const MOVIE_ATTRIBUTE_SET_ID = '9';
+    const MOVIE_CATEGORY_ID = '3';
+}
---
+++ app/code/Cadence/Movie/registration.php
@@ -0,0 +1,7 @@
+<?php
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Cadence_Movie',
+    __DIR__
+);
